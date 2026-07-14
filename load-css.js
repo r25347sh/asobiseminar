360ffdc90519d47d1437ba2bfde65f0e0795be62
r25347sh <r25347sh@hs.reitaku.jp>
2026-07-14 19:35:55 +0900

    Update load-css.js

--- load-css.js
+++ load-css.js
@@ -5,10 +5,9 @@ const cssFiles = [
   '/asobiseminar/hamburgernav.css?v=2', // キャッシュ対策もバッチリ
 ];
 
-// ループ処理でHTMLの<head>の中に<link>タグを同時にぶち込む
-cssFiles.forEach(url => {
+cssFiles.reverse().forEach(url => {
   const link = document.createElement('link');
   link.rel = 'stylesheet';
   link.href = url;
-  document.head.appendChild(link); // 並行して一瞬でダウンロードが始まる
+  document.head.prepend(link);
 });
