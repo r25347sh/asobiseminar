1d0590afe21096d504a3dadd958a6be31d2de189
r25347sh <r25347sh@hs.reitaku.jp>
2026-07-16 08:38:24 +0900

    Update load-css.js

--- js/load-css.js
+++ js/load-css.js
@@ -1,18 +1,16 @@
 // js/load-css.js
 (function() {
-  // 1. 全ページで100%絶対に使う「共通の土台CSS」
   const cssFiles = [
     '/asobiseminar/gaibu/unpkg.css',
     '/asobiseminar/css/style.css?v=2026'
   ];
 
-  // 2. ブラウザの現在のURLをチェックして、専用CSSを自動追加
   const currentPath = window.location.pathname;
 
   if (currentPath.includes('members.html')) {
     cssFiles.push('/asobiseminar/css/members.css?v=1');
   } 
-  else if (currentPath.includes('groups/')) {
+  else if (currentPath.includes('/groups/')) {
     cssFiles.push('/asobiseminar/css/groupsIndex.css?v=1');
   } 
   else if (currentPath.includes('aboutsite.html')) {
@@ -22,15 +20,13 @@
     cssFiles.push('/asobiseminar/css/settings.css?v=1');
   } 
   else if (currentPath.includes('programmer.html')) {
-    // ★ 新規追加：マークアッププログラマー専用CSS
-    cssFiles.push('/asobiseminar/css/programmer.css?v=1');
+    cssFiles.push('/asobiseminar/css/programmer.css?v=2026');  // ★ 確実ロード
   } 
   else {
-    // トップページなど
     cssFiles.push('/asobiseminar/css/index-main.css?v=1');
   }
 
-  // 3. すべてのCSSを逆順で読み込み（専用CSSが優先）
+  // 逆順で優先順位を確保
   cssFiles.reverse().forEach(url => {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
