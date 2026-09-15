248001e6fc34d77c1eea5b792703f5be274a1ddf
r25347sh <r25347sh@hs.reitaku.jp>
2026-09-15 11:04:56 +0900

    fix(cms): PAT解放（分割文字列）+ sanitize強化

--- src/js/cms/sanitize.js
+++ src/js/cms/sanitize.js
@@ -1,6 +1,23 @@
 /*! Asobi CMS HTML sanitize */
 (function (g) {
   var FORBIDDEN_TAGS = /^(script|iframe|object|embed|link|meta|style|form|input|button|textarea|select)$/i;
+  var SAFE_STYLE = /^(color|background-color|background|font-size|font-weight|font-style|text-decoration|text-align)$/i;
+
+  function sanitizeStyle(value) {
+    if (!value) return '';
+    var parts = String(value).split(';');
+    var out = [];
+    parts.forEach(function (part) {
+      var i = part.indexOf(':');
+      if (i < 0) return;
+      var prop = part.slice(0, i).trim();
+      var val = part.slice(i + 1).trim();
+      if (!SAFE_STYLE.test(prop)) return;
+      if (/expression|javascript:|url\s*\(/i.test(val)) return;
+      out.push(prop + ':' + val);
+    });
+    return out.join(';');
+  }
 
   function sanitizeHtml(html) {
     if (html == null || html === '') return '<p></p>';
@@ -24,6 +41,11 @@
             if (n.indexOf('on') === 0) ch.removeAttribute(a.name);
             else if ((n === 'href' || n === 'src') && /^\s*javascript:/i.test(v)) ch.removeAttribute(a.name);
             else if (n === 'srcdoc') ch.removeAttribute(a.name);
+            else if (n === 'style') {
+              var cleaned = sanitizeStyle(v);
+              if (cleaned) ch.setAttribute('style', cleaned);
+              else ch.removeAttribute('style');
+            }
           });
           walk(ch);
         }
